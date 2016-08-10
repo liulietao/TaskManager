d0a20ba09e8a6f6ef50d5b1d3d84531da0136648
liulietao <jacksonovo@163.com>
2016-08-10 18:58:45 +0800

    add log, OSUtil

--- src/main/java/com/youku/opencloud/TaskManager/TaskProducer.java
+++ src/main/java/com/youku/opencloud/TaskManager/TaskProducer.java
@@ -12,16 +12,21 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.apache.zookeeper.*;
 
+import com.youku.opencloud.Util.OSUtils;
+
 /**
  * @author liulietao
  *
  */
 public class TaskProducer implements Watcher, Closeable {
 	
-	private static final Logger LOG = LoggerFactory.getLogger(TaskProducer.class);
+	private static final Logger log = LoggerFactory.getLogger(TaskProducer.class);
 	
 	private ZooKeeper zk;
 	private String hostPort;
+	
+	private volatile boolean connected = false;
+	private volatile boolean expired = false;
 
 	/**
 	 * Create a new task producer
@@ -39,6 +44,23 @@ public class TaskProducer implements Watcher, Closeable {
 		zk.close();
 	}
 	
+    /**
+     * Check if this client is connected.
+     * 
+     * @return boolean ZooKeeper client is connected
+     */
+    boolean isConnected() {
+        return connected;
+    }
+    
+    /**
+     * Check if the ZooKeeper session has expired.
+     * 
+     * @return boolean ZooKeeper session has expired
+     */
+    boolean isExpired() {
+        return expired;
+    }	
 	
 	/* (non-Javadoc)
 	 * @see java.io.Closeable#close()
@@ -46,24 +68,54 @@ public class TaskProducer implements Watcher, Closeable {
 	@Override
 	public void close() throws IOException {
 		// TODO Auto-generated method stub
-
+		log.info("close");
 	}
 
 	/* (non-Javadoc)
 	 * @see org.apache.zookeeper.Watcher#process(org.apache.zookeeper.WatchedEvent)
 	 */
 	@Override
-	public void process(WatchedEvent arg0) {
+	public void process(WatchedEvent e) {
 		// TODO Auto-generated method stub
-		LOG.info("process");
+		log.info("process event, type:{}, state:{}", e.getType(), e.getState());
+		
+		if (e.getType() == Event.EventType.None) {
+			switch (e.getState()) {
+			case SyncConnected:
+				connected = true;
+				break;
+			case Disconnected:
+				connected = false;
+				break;
+			case Expired:
+				expired = true;
+				connected = false;
+				log.error("session expired");
+				break;
+			default:
+				break;
+			}
+		}
 	}
 
 	/**
 	 * @param args
+	 * @throws IOException 
+	 * @throws InterruptedException 
 	 */
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+	@SuppressWarnings("resource")
+	public static void main(String[] args) throws IOException, InterruptedException {
+		TaskProducer p = new TaskProducer(args[0]);
+		p.startZK();
+		
+		while (! p.isConnected()) {
+			Thread.sleep(100);
+		}
+		
+		int cpuLoad = OSUtils.cpuUsage();
+		log.info("cpu load : {}", cpuLoad);
+		
+		p.stopZK();
 	}
 
 }
